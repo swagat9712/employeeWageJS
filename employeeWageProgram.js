c2b734ba7f870e9e80c9575b4137fdb25c641f52
Swagat Bhattacharjee <66122924+swagat1297@users.noreply.github.com>
2020-11-17 09:46:38 +0530

    Refactored: Assuming two variables

--- employeeWageProgram.js
+++ employeeWageProgram.js
@@ -1,4 +1,6 @@
 console.log("Welcome to EmployeeWage Computation!");
+const WagePerHour = 20;
+const FullDay = 8;
 //to check attendance//
 function checkRandomValue() {
     return (Math.ceil(Math.random() * 10) % 3 + 1);
