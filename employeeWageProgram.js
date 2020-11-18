5989a9d639c73df3b1ff2233c14fb680fc6efae6
Swagat Bhattacharjee <66122924+swagat1297@users.noreply.github.com>
2020-11-18 11:49:38 +0530

    Added: main file

--- employeeWageProgram.js
+++ employeeWageProgram.js
@@ -1,7 +1,3 @@
+let utility = require("./employeeWageUtility.js")
 console.log("Welcome to EmployeeWage Computation!");
-const WagePerHour = 20;
-const FullDay = 8;
-//to check attendance//
-function checkRandomValue() {
-    return (Math.ceil(Math.random() * 10) % 3 + 1);
-}
+utility.computeEmpWagesValues();
